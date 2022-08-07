57041186a26d472f8bb79d90196c07c6c60c16e8
Cristian <cris18002011@hotmail.com>
2022-08-06 23:40:32 -0500

    Fusión carrito de compras

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const desktopMenu = document.querySelector(".desktopMenu");
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobileMenu");
 
+const shoppingCardIcon = document.querySelector(".navbarShoppingCard");
+const shoppingCardArrow = document.querySelector(".arrow");
+const shoppingCardMenu = document.querySelector(".productDetail");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
+shoppingCardIcon.addEventListener("click", toggleCartMenu);
+shoppingCardArrow.addEventListener("click", toggleCartMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
@@ -13,4 +19,10 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+  shoppingCardMenu.classList.add("inactive");
+}
+
+function toggleCartMenu() {
+  shoppingCardMenu.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
 }
